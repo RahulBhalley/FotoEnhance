99f216963326f31d4b25e7b10f2ca3045d65e5ca
Rahul Bhalley <0behinds_secant@icloud.com>
2022-07-12 11:33:23 +0530

    Initial Commit

---
+++ FotoEnhance/FotoEnhanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FotoEnhanceApp.swift
+//  FotoEnhance
+//
+//  Created by Rahul Bhalley on 12/07/22.
+//
+
+import SwiftUI
+
+@main
+struct FotoEnhanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
