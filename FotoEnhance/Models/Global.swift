f1b0a098d454c1c3f7adf85e9fa71f4700d0afa9
Rahul Bhalley <0behinds_secant@icloud.com>
2023-04-18 19:54:37 +0530

    Added `Global` variables.

---
+++ FotoEnhance/Models/Global.swift
@@ -0,0 +1,23 @@
+//
+//  Global.swift
+//  FotoEnhance
+//
+//  Created by Rahul Bhalley on 18/04/23.
+//
+
+import Foundation
+
+struct Global {
+    
+    // MARK: App information
+    
+    static let appName = "FotoEnhance"
+    static let appPageUrl = URL(string: "https://itunes.apple.com/app/id1633937151")!
+    static let appReviewUrl = URL(string: "https://itunes.apple.com/app/id1633937151?action=write-review")!
+    static var appVersion: String {
+        let dictionary = Bundle.main.infoDictionary!
+        let version = dictionary["CFBundleShortVersionString"] as! String
+        let build = dictionary["CFBundleVersion"] as! String
+        return version + "(" + build + ")"
+    }
+}
