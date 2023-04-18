5d1abff87d0683622f4dfefa0dc7990b7bfc1d97
Rahul Bhalley <0behinds_secant@icloud.com>
2023-04-18 19:53:23 +0530

    Trying to get more area around face for face enhancement.

--- FotoEnhance/DeepLearning/DetectFaces.swift
+++ FotoEnhance/DeepLearning/DetectFaces.swift
@@ -12,7 +12,8 @@ import Vision
 func faceDetectedAndBoundingBoxes(in inputImage: inout UIImage) -> (Bool, [CGRect]) {
     
     // Create a request handler.
-    let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: inputImage.pixelBuffer!)
+    let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: inputImage.pixelBuffer!,
+                                                    orientation: .up)
     
     // Create a face detection request.
     let request = VNDetectFaceRectanglesRequest()
@@ -31,13 +32,35 @@ func faceDetectedAndBoundingBoxes(in inputImage: inout UIImage) -> (Bool, [CGRec
     }
     
     for (index, face) in results.enumerated() {
-        print("face \(index):")
-        print("normalizedCoordinates: \(face.boundingBox):")
-        print("newCoordinates: \(transformNormalizedToImageCoordinates(from: face.boundingBox, to: inputImage.size))")
+        print("face \(index).")
+        print("normalizedCoordinates: \(face.boundingBox).")
+        print("newCoordinates: \(transformNormalizedToImageCoordinates(from: face.boundingBox, to: inputImage.size)).")
     }
     guard let cgImage = inputImage.cgImage else { return (false, []) }
     if !results.isEmpty {
-        inputImage = UIImage(cgImage: cgImage.cropping(to: transformNormalizedToImageCoordinates(from: results[0].boundingBox, to: inputImage.size))!)
+        // Get more area around detected face to cover hair as well.
+        var faceCoordinates = transformNormalizedToImageCoordinates(from: results[0].boundingBox, to: inputImage.size)
+        print("BEFORE")
+        print("faceCoordinates.origin: \(faceCoordinates.origin)")
+        print("faceCoordinates.size: \(faceCoordinates.size)")
+        
+        let moreAreaPercent = 0.4
+        
+        // Calculate new origin and size.
+        let newX = faceCoordinates.minX - (faceCoordinates.width * moreAreaPercent)
+        let newY = faceCoordinates.minY - (faceCoordinates.height * moreAreaPercent)
+        let newWidth = faceCoordinates.width + 2 * (faceCoordinates.width * moreAreaPercent)
+        let newHeight = faceCoordinates.height + 2 * (faceCoordinates.height * moreAreaPercent)
+        
+        // Set new origin nd size.
+        faceCoordinates.origin = CGPoint(x: newX, y: newY)
+        faceCoordinates.size = CGSize(width: newWidth, height: newHeight)
+        print("AFTER")
+        print("faceCoordinates.origin: \(faceCoordinates.origin)")
+        print("faceCoordinates.size: \(faceCoordinates.size)")
+        print("image.size: \(inputImage.size)")
+        
+        inputImage = UIImage(cgImage: cgImage.cropping(to: faceCoordinates)!)
         print(inputImage.size)
     }
     
@@ -55,7 +78,7 @@ func transformNormalizedToImageCoordinates(from normalizedCoordinates: CGRect, t
     var newCoordinates = CGRect()
     newCoordinates.size.width = normalizedCoordinates.size.width * imageCoordinates.size.width
     newCoordinates.size.height = normalizedCoordinates.size.height * imageCoordinates.size.height
-    newCoordinates.origin.y = (imageCoordinates.height) - (imageCoordinates.height * normalizedCoordinates.origin.y )
+    newCoordinates.origin.y = (imageCoordinates.height) - (imageCoordinates.height * normalizedCoordinates.origin.y)
     newCoordinates.origin.y = newCoordinates.origin.y - newCoordinates.size.height
     newCoordinates.origin.x = normalizedCoordinates.origin.x * imageCoordinates.size.width
     return newCoordinates
